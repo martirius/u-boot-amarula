5d51aa85542ce0282696d39b51b33d40985dd10f
Jagan Teki <jagan@amarulasolutions.com>
2019-06-15 19:49:05 +0530

    ram: rk3399: Handle pctl_cfg return type
    Add proper return type handling of pctl_cfg with
    meaningful print statement.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/ram/rockchip/sdram_rk3399.c
+++ drivers/ram/rockchip/sdram_rk3399.c
@@ -1119,6 +1119,7 @@ static int sdram_init(struct dram_info *dram,
 	unsigned char dramtype = params->base.dramtype;
 	unsigned int ddr_freq = params->base.ddr_freq;
 	int channel;
+	int ret;
 
 	debug("Starting SDRAM initialization...\n");
 
@@ -1138,9 +1139,10 @@ static int sdram_init(struct dram_info *dram,
 		if (channel >= params->base.num_channels)
 			continue;
 
-		if (pctl_cfg(chan, channel, params) != 0) {
-			printf("pctl_cfg fail, reset\n");
-			return -EIO;
+		ret = pctl_cfg(chan, channel, params);
+		if (ret < 0) {
+			printf("%s: pctl config failed\n", __func__);
+			return ret;
 		}
 
 		/* LPDDR2/LPDDR3 need to wait DAI complete, max 10us */
